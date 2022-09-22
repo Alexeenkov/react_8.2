0030f5f2e23fd97a8d54886101a816235401e94f
Илья Юрьевич Алексеенков <74237440+Alexeenkov@users.noreply.github.com>
2022-09-22 08:10:33 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import {Data} from './components/Data';
+import {Error} from './components/Error';
+import {Loading} from './components/Loading';
+
+
+function App() {
+  return (
+   <>
+    <Data />
+    <Error />
+    <Loading />
+   </>
+  );
+}
+
+export default App;
